711626423a976881f3015cb374de5ac7a1c46ad5
aash29 <aash29@gmail.com>
2016-10-12 01:19:46 +0300

    + descriptions

---
+++ descriptions.lua
@@ -0,0 +1,24 @@
+globals = {
+	wasteland_dsc_visited ="мы тут уже были"
+}
+function constructDescription(node)
+
+end
+wasteland_descriptions = {
+ ["1,1"] = 
+	{
+		angry = '',
+		neutral = '',
+		neutral_night = '',
+		kindly_morning = '',
+		kindly_day = '',
+		kindly_evening = '',
+		kindly_night = '',
+		insult = '',
+		comment = '',
+		subdirections = '',
+		visited = '',
+		extra = '',
+		extra_rule = [[code]]
+		}
+	};
