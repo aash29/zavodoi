3f852cd617f7dd2f816b7b8a9948792cab873949
aash29 <aash29@gmail.com>
2016-10-26 23:06:42 +0300

    random harpies

--- forest.lua
+++ forest.lua
@@ -57,36 +57,103 @@ end
 harpies = list {}
 
 
+harpiesAct = function(s)
+        p( s.replies[s.cr]);
+        s.cr=s.cr+1
+        if (s.cr > #s.replies) then
+                walk(harpiesAttack)
+        end
+end
+
 
 
 harpies[1] = new [[obj {
-        nam = 'часы',
+        nam = 'harpy1',
         replies={"ответ 1 гарпии 1",
         "ответ 2 гарпии 1",
         "ответ 3 гарпии 1"},
         --replies[2] = "ответ 2 гарпии 1",
         cr = 1,
         dsc = '{Гарпия №1 }',
-        act = function(s)
-                p( s.replies[s.cr]);
-                s.cr=s.cr+1
-        end
+        act = harpiesAct
     } ]] 
 
 
 
+harpies[2] = new [[obj {
+        nam = 'harpy2',
+        replies={"ответ 1 гарпии 2",
+        "ответ 2 гарпии 2",
+        "ответ 3 гарпии 2"},
+        cr = 1,
+        dsc = '{Гарпия №2 }',
+        act = harpiesAct
+    } ]] 
+
+
+harpies[3] = new [[obj {
+        nam = 'harpy3',
+        replies={"ответ 1 гарпии 3",
+        "ответ 2 гарпии 3"},
+        cr = 1,
+        dsc = '{Гарпия №3 }',
+        act = harpiesAct
+    } ]] 
+
+harpies[4] = new [[obj {
+        nam = 'harpy4',
+        replies={"ответ 1 гарпии 4",
+        "ответ 2 гарпии 4",
+        "ответ 3 гарпии 4",
+        "ответ 4 гарпии 4"},
+        cr = 1,
+        dsc = '{Гарпия №4 }',
+        act = harpiesAct
+    } ]] 
+
+harpies[5] = new [[obj {
+        nam = 'harpy5',
+        replies={"ответ 1 гарпии 5"},
+        cr = 1,
+        dsc = '{Гарпия №5 }',
+        act = harpiesAct
+    } ]] 
+
+
+harpiesAttack = room {
+    nam = 'Гарпии напали';
+    dsc = 'Гарпии напали';
+}
 
 harpiesScene = room {
     nam = 'Гарпии';
     dsc = 'Кругом гарпии';
     entered = function (s, f)
+
+        local indices = {}
+        for i = 1, 5 do
+            indices[#indices+1] = i
+        end
+
+        numHarpiesInScene=3
+
         objs():zap()
-        objs():add(harpies[1])
+
+        for i = 1, numHarpiesInScene do
+            local index = math.random(#indices)
+        -- remove it from the list so it won't be used again
+            objs():add(harpies[indices[index]])
+            table.remove(indices, index)
+
+        end
+
+
+        
     end;
     left = function (s, f)
     end;
     obj={};
     way = {};
-    }
+}
 
 
